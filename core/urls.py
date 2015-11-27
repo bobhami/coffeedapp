6deb5af61c0786067cafef75f3e43bf69ecdca6a
Bob Hamilton <Home@Bobs-MacBook-Air.local>
2015-11-27 10:19:38 -0500

    added previously untracked files

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import core.views as coreviews
+
+urlpatterns = patterns('',
+	url(r'^$', coreviews.LandingView.as_view()),
+)
